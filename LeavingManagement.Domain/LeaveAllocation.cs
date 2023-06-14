6a583daa3a5c6a425d8d7fc6c7a5cf2e0e94b199
João Marcos Cerqueira <joaombcerqueira@gmail.com>
2023-06-13 23:42:52 -0300

    Adicionar arquivos de projeto.

---
+++ LeavingManagement.Domain/LeaveAllocation.cs
@@ -0,0 +1,11 @@
+﻿using LeavingManagement.Domain.Common;
+
+namespace LeavingManagement.Domain;
+
+public class LeaveAllocation : BaseEntity
+{
+    public int NumberOfDays { get; set; }
+    public LeaveType? LeaveType { get; set; }
+    public int LeaveTypeId { get; set; }
+    public int Period { get; set; }
+}
---
+++ LeavingManagement.Domain/LeaveRequest.cs
@@ -0,0 +1,20 @@
+﻿using LeavingManagement.Domain.Common;
+
+namespace LeavingManagement.Domain;
+
+public class LeaveRequest : BaseEntity
+{
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+
+    public LeaveType? LeaveType { get; set; }
+    public int LeaveTypeId { get; set; }
+
+    public DateTime DateRequested { get; set; }
+    public string? RequestComments { get; set; }
+
+    public bool? Approved { get; set; }
+    public bool Cancelled { get; set; }
+
+    public string RequestingEmployeeId { get; set; } = string.Empty;
+}
