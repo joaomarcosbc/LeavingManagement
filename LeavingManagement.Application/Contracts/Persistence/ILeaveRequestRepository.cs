61918241c157dc554eb91dbd15b9768f0c11b7f3
João Marcos Cerqueira <joaombcerqueira@gmail.com>
2023-06-15 18:10:44 -0300

    feat: Implements Contracts on Application Project

---
+++ LeavingManagement.Application/Contracts/Persistence/ILeaveRequestRepository.cs
@@ -0,0 +1,9 @@
+﻿using LeavingManagement.Domain;
+
+namespace LeavingManagement.Application.Contracts.Persistence
+{
+    public interface ILeaveRequestRepository : IGenericRepository<LeaveRequest>
+    {
+
+    }
+}
