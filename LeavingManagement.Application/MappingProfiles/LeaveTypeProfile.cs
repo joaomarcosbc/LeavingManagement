7092cf32218dc428cf293bf5021d4f53b4169595
João Marcos Cerqueira <joaombcerqueira@gmail.com>
2023-06-16 22:29:41 -0300

    feat: adds AutoMapper and MediatR

---
+++ LeavingManagement.Application/MappingProfiles/LeaveTypeProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using LeavingManagement.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeavingManagement.Application.MappingProfiles
+{
+    public class LeaveTypeProfile : Profile
+    {
+        public LeaveTypeProfile()
+        {
+            // CreateMap<LeaveTtypeDto, LeaveType>().ReverseMap();
+        }
+    }
+}
