ca041cefebe4893ff535536e81a8d044c54c8353
João Marcos Cerqueira <joaombcerqueira@gmail.com>
2023-06-25 00:00:47 -0300

    feat: Implements DeleteLeaveTypeDetails query feature

---
+++ LeavingManagement.Application/Features/LeaveType/Commands/DeleteLeaveType/DeleteLeaveTypeCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using LeavingManagement.Application.Contracts.Persistence;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeavingManagement.Application.Features.LeaveType.Commands.DeleteLeaveType
+{
+    public class DeleteLeaveTypeCommandHandler : IRequestHandler<DeleteLeaveTypeCommand, Unit>
+    {
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+
+        public DeleteLeaveTypeCommandHandler(ILeaveTypeRepository leaveTypeRepository)
+        {
+            _leaveTypeRepository = leaveTypeRepository;
+        }
+        public async Task<Unit> Handle(DeleteLeaveTypeCommand request, CancellationToken cancellationToken)
+        {
+
+            var leaveTypeToDelete = await _leaveTypeRepository.GetByIdAsync(request.Id);
+
+            // verify if record exists
+
+            await _leaveTypeRepository.DeleteAsync(leaveTypeToDelete);
+
+            return Unit.Value;
+        }
+    }
+}
