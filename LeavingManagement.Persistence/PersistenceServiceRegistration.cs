f8da559c00027d269da77b26f5db56d16e85a7d8
João Marcos Cerqueira <joaombcerqueira@gmail.com>
2023-07-22 22:01:22 -0300

    feat: setup Persistence project

---
+++ LeavingManagement.Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeavingManagement.Persistence
+{
+    public static class PersistenceServiceRegistration
+    {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
