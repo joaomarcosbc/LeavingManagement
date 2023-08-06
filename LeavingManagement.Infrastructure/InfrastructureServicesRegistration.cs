f351b8e901ab4ef84424c68fef940891dfc84005
João Marcos Cerqueira <joaombcerqueira@gmail.com>
2023-08-06 00:07:48 -0300

    feat: Creates Infrastrucutre project

---
+++ LeavingManagement.Infrastructure/InfrastructureServicesRegistration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace LeavingManagement.Infrastructure
+{
+    public static class InfrastructureServicesRegistration
+    {
+        public static IServiceCollection ConfigureInfrastuctureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services;
+        }
+    }
+}
