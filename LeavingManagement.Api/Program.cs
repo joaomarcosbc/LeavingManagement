5f788cc5a8f0802731a3119599cd523ee73dcff2
João Marcos <joaombcerqueira@gmail.com>
2023-09-21 22:15:23 -0300

    feat: Configures Web Api Project

--- LeavingManagement.Api/Program.cs
+++ LeavingManagement.Api/Program.cs
@@ -1,8 +1,24 @@
+using LeavingManagement.Application;
+using LeavingManagement.Persistence;
+using LeavingManagement.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddApplicationServices();
+builder.Services.ConfigureInfrastuctureServices(builder.Configuration);
+builder.Services.AddPersistenceServices(builder.Configuration);
+
 builder.Services.AddControllers();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("all", corsPolicyBuilder => corsPolicyBuilder.AllowAnyOrigin()
+        .AllowAnyHeader()
+        .AllowAnyMethod());
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
