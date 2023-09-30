582fa2a2c2b1b721c688b52afa29ff305585a3b1
João Marcos <joaombcerqueira@gmail.com>
2023-09-29 21:26:00 -0300

    feat: Adds EF Tools

--- LeavingManagement.Application/Models/Email/EmailSettings.cs
+++ LeavingManagement.Application/Models/Email/EmailSettings.cs
@@ -3,7 +3,7 @@
     public class EmailSettings
     {
         public string ApiKey { get; set; } = string.Empty;
-        public string FromAdress { get; set; } = string.Empty;
+        public string FromAddress { get; set; } = string.Empty;
         public string FromName { get; set; } = string.Empty;
     }
 }
